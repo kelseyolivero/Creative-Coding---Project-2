04c82933dbc85593ed75cb7c70832e8ee0e55ad3
kelseyolivero <kelseyo1998@hotmail.co.uk>
2020-04-18 15:39:23 +0200

    update

---
+++ sketch2.js
@@ -0,0 +1,16 @@
+function setup() {
+  song = loadSound('audio/forest.mp3');
+  createCanvas(720, 200);
+  background(255, 0, 0);
+}
+
+function mousePressed() {
+  if (song.isPlaying()) {
+    // .isPlaying() returns a boolean
+    song.stop();
+    background(255, 0, 0);
+  } else {
+    song.play();
+    background(0, 255, 0);
+  }
+}
